a98670cd9479b2618c3cace2b6b9798f75a47ccc
seoh77 <seoh7dev@gmail.com>
2023-09-26 22:49:18 +0900

    5-4 React에서 배열 사용하기2 - 데이터 추가하기

--- simple-diary/src/App.js
+++ simple-diary/src/App.js
@@ -1,36 +1,54 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-const dummyList = [
-  {
-    id: 1,
-    author: "사람1",
-    content: "hi 1",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "사람2",
-    content: "hi 2",
-    emotion: 2,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "사람3",
-    content: "hi 3",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-];
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "사람1",
+//     content: "hi 1",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "사람2",
+//     content: "hi 2",
+//     emotion: 2,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "사람3",
+//     content: "hi 3",
+//     emotion: 3,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setDate] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setDate([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
