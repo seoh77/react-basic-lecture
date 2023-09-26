21fb230155ac52e76c03ddfd3dc4bcd8ccc632d5
seoh77 <seoh7dev@gmail.com>
2023-09-26 13:10:23 +0900

    5-1 React에서 사용자 입력 처리하기

---
+++ simple-diary/src/DiaryEditor.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log(state);
+    alert("저장 성공");
+  };
+
+  return (
+    // 최상위 태그의 클래스 네임을 컴포넌트 네임과 똑같이 맞춰주기
+    // -> CSS의 클래스를 가지고 스타일링을 할 때 클래스의 이름과 컴포넌트의 이름을 정확히 일치시켜 직관적으로 스타일 코드를 작성하기 위함
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        {/* input의 입력 값이 바뀌면(onChange) 실시간으로 상태 변화 함수 setAuthor를 이용해서 author state에 입력값을 저장 */}
+        <input
+          name="author"
+          value={state.author}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <textarea
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <span>오늘의 감정점수 : </span>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
