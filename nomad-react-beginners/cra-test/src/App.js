45515b8de83b4accf1f32e1fb8db6bd6029c2b04
seoh77 <seoh7dev@gmail.com>
2023-10-03 21:32:21 +0900

    #6.0-6.2 useEffect

--- nomad-react-beginners/cra-test/src/App.js
+++ nomad-react-beginners/cra-test/src/App.js
@@ -1,11 +1,46 @@
-import Button from "./Button";
-import style from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  // 항상 실행
+  console.log("I run all the time");
+
+  // 처음 한 번만 실행
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+
+  // keyword가 변경될 때만 실행
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("I run when 'keyword' changes.");
+    }
+  }, [keyword]);
+
+  // counter가 변경될 때만 실행
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  });
+
+  // keyword 또는 counter가 변경되면 실행
+  useEffect(() => {
+    console.log("I run when keyword & counter change.");
+  });
+
   return (
     <div>
-      <h1 className={style.title}>Welcome!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
