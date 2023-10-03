29d7f6ec426c29305a191c962ef559e46b163363
seoh77 <seoh7dev@gmail.com>
2023-10-03 21:49:06 +0900

    #6.4 Cleanup

--- nomad-react-beginners/cra-test/src/App.js
+++ nomad-react-beginners/cra-test/src/App.js
@@ -1,46 +1,20 @@
 import { useEffect, useState } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  // 항상 실행
-  console.log("I run all the time");
-
-  // 처음 한 번만 실행
+function Hello() {
   useEffect(() => {
-    console.log("I run only once.");
+    console.log("hi");
+    return () => console.log("bye");
   }, []);
+  return <h1>Hello</h1>;
+}
 
-  // keyword가 변경될 때만 실행
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("I run when 'keyword' changes.");
-    }
-  }, [keyword]);
-
-  // counter가 변경될 때만 실행
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  });
-
-  // keyword 또는 counter가 변경되면 실행
-  useEffect(() => {
-    console.log("I run when keyword & counter change.");
-  });
-
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "show"}</button>
     </div>
   );
 }
