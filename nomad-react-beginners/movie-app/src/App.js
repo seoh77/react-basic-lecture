396256102ce52f3ff339dbd4a01bc7cd63694a9e
seoh77 <seoh7dev@gmail.com>
2023-10-30 22:24:20 +0900

    #7.5 React Router

--- nomad-react-beginners/movie-app/src/App.js
+++ nomad-react-beginners/movie-app/src/App.js
@@ -1,5 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
+
+// function App() {
+//   return (
+//     <Router>
+//       <Route path="/movie">
+//         <Detail />
+//       </Route>
+//       <Route path="/">
+//         <Home />
+//       </Route>
+//     </Router>
+//   );
+// }
 
 export default App;
